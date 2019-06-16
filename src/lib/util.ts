7260886f9d0091a88de7de1ea38c686b09442b4e
Noah Smith <noah@batteredoats.com>
2019-06-16 14:46:47 -0400

    refactoring frame buffer out of render

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -15,15 +15,15 @@ export function fixCanvas(canvas: HTMLCanvasElement) {
   }
 }
 
-export function drawBody(ctx: CanvasRenderingContext2D, seg: FrameSegment) {
+export function drawArc(ctx: CanvasRenderingContext2D, seg: Arc) {
   ctx.beginPath();
-  ctx.arc(seg.line[1].x, seg.line[1].y, 4, 0, 2 * Math.PI);
+  ctx.arc(seg.c.x, seg.c.y, 4, 0, 2 * Math.PI);
   ctx.closePath();
   ctx.fillStyle = seg.fillStyle;
   ctx.fill();
 }
 
-export function drawPath(ctx: CanvasRenderingContext2D, seg: FrameSegment) {
+export function drawPath(ctx: CanvasRenderingContext2D, seg: Path) {
   ctx.beginPath();
   ctx.moveTo(seg.line[0].x, seg.line[0].y);
   ctx.lineTo(seg.line[1].x, seg.line[1].y);
@@ -43,11 +43,19 @@ async function waitForIt() {
   });
 }
 
-interface FrameSegment {
+interface Path {
+  type: string;
   line: Array<Point>;
   strokeStyle: string;
+}
+
+interface Arc {
+  type: string;
+  c: Point;
   fillStyle: string;
 }
+
+type FrameSegment = Path | Arc;
 
 interface Frame {
   [index: number]: FrameSegment;
@@ -55,39 +63,85 @@ interface Frame {
   forEach(callback: Function): void;
 }
 
+class Buffer {
+  private _buff: Array<any>;
+  public length: number;
+  public bufferedLength: number;
+
+  constructor(public maxLength: number) {
+    this._buff = [];
+    this.length = 0;
+    this.bufferedLength = 0;
+  }
+
+  last() {
+    return this._buff[this._buff.length - 1];
+  }
+
+  add(el: any): number {
+    const length = this._buff.push(el);
+
+    if (length > this.maxLength) {
+      this._buff.shift();
+    }
+
+    this.length++;
+    this.bufferedLength = this._buff.length;
+
+    return this.length;
+  }
+
+  pairs() {
+    if (this._buff.length >= 1) {
+      return this._buff.slice(1).reduce((m, el, i) => {
+        m.push([this._buff[i], el]);
+        return m;
+      }, []);
+    }
+  }
+}
+
 // turn this into an iterator that returns frames
-// instead of passing a frame buffer back to render, calculate the frame buffer
-// here and flatten it as frame segements
 export function calculateFrames(moves: Collider, maxFrames: number = 1000) {
-  let colliders: Array<Collider> = [];
+  let colliders = new Buffer(10);
   let frames: Array<Frame> = [];
-  let i = 0;
 
   for (let m1 of moves) {
     if (!m1) continue;
-    else if (i >= 1) {
-      let m0 = colliders[i - 1];
 
-      const frame = m1.bodies.map((body: GBody, i: number) => {
-        const magnitude = body.vec.getMagnitude();
-        const magByte = (magnitude / 10) * 255;
-        const p0 = m0.bodies[i].pos;
-        const p1 = body.pos;
+    colliders.add(m1);
 
+    if (colliders.length >= 1) {
+      const bodies = m1.bodies.map((body: GBody, i: number) => {
         return {
-          line: [p0, p1],
-          strokeStyle: `rgb(${magByte}, 0, 0)`,
-          fillStyle: `rgba(0, 0, 0, .1)`
+          type: "Arc",
+          c: body.pos,
+          fillStyle: "rgba(0, 0, 0, .1)"
         };
       });
 
-      frames.push(frame);
+      const paths = colliders
+        .pairs()
+        .reduce((frame: Array<FrameSegment>, [_m0, _m2]: Array<Collider>) => {
+          const segments = _m2.bodies.map((body: GBody, i: number) => {
+            const magnitude = body.vec.getMagnitude();
+            const magByte = (magnitude / 10) * 255;
+            const p0 = _m0.bodies[i].pos;
+            const p1 = body.pos;
+
+            return {
+              type: "Path",
+              line: [p0, p1],
+              strokeStyle: `rgb(${magByte}, 0, 0)`
+            };
+          });
+          return [...frame, ...segments];
+        }, []);
+
+      frames = [...frames, [...bodies, ...paths]];
     }
 
-    colliders.push(m1);
-    i++;
-
-    if (i === maxFrames) break;
+    if (colliders.length === maxFrames) break;
   }
 
   return frames;
@@ -98,15 +152,14 @@ export async function render(
   frames: Array<Frame>
 ) {
   for (let i = 0; i < frames.length; i++) {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     const currentFrame = frames[i];
-    const frameBuffer = frames.slice(Math.max(0, i - 10), i);
 
-    currentFrame.forEach((seg: FrameSegment) => drawBody(ctx, seg));
-
-    frameBuffer.forEach((f: Frame) => {
-      f.forEach((seg: FrameSegment) => drawPath(ctx, seg));
-    });
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const draw = {
+      Arc: (ctx: CanvasRenderingContext2D, seg: Arc) => drawArc(ctx, seg),
+      Path: (ctx: CanvasRenderingContext2D, seg: Path) => drawPath(ctx, seg)
+    };
+    currentFrame.forEach((seg: FrameSegment) => draw[seg.type](ctx, seg));
 
     await waitForIt();
   }
